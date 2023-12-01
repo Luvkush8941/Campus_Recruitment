720000c2ac5d9a491a45f35ea9c338405b78a5cf
Luvkush sharma <105363463+Luvkush8941@users.noreply.github.com>
2023-12-01 11:03:44 +0530

    Update signUp.js

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -5,7 +5,7 @@ const signup = async (userDetails) => {
       const result = await axios({
   
         method: 'POST',
-        url: 'http://127.0.0.1:3000/api/v1/users/signup',
+        url: '/api/v1/users/signup',
         
         // Sending data 
         data : {
