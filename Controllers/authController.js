8c101e24b1a0a87bf37a76182eb33ad47c18c5be
Luvkush sharma <105363463+Luvkush8941@users.noreply.github.com>
2023-12-01 10:18:55 +0530

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -347,8 +347,6 @@ exports.updatePassword = async (req , res , next) => {
     
    } catch (err) {
 
-    console.log(err);
-
     res.status(500).json ({
 
        status : 'fail',
@@ -384,5 +382,4 @@ exports.deleteMe = async (req , res , next) => {
       })
    }
 }
-
 
